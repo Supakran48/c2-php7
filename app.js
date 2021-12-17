0f9b1892988e00107a04a6f6b12c02aa28abcf36
Supakran48 <flukeark1493@gmail.com>
2021-12-17 10:51:00 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+function multiple(x,y){
+    console.log('Hello:x:${x},y:${Y}');
+}
+multiple(20,10);
---
+++ php701.php
@@ -0,0 +1,7 @@
+<?php
+//Define a function
+function multiple($x,$y){
+    echo"Hello World.:x= $x,y=$y<br />";
+}
+//Function call
+multiple(20,10);
